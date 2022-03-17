b283db2f86ab6dd5889f55a6f6bc530c8d59b2b1
raphTec <git@raphtec.net>
2022-03-17 15:32:04 +0100

    Fix css filenames instead of concatting a big css

--- command.sh
+++ command.sh
@@ -1,5 +1,9 @@
+# needs Arch package perl-rename
+
 # this is ridiculous - how many programs do you need to archive a forum, fast?
 output_dir="$HOME/fusornet"
 RUST_BACKTRACE=1 cargo run -- http://fusor.net --jobs 16 --verbose --output $output_dir --exclude-download '(?:view=print)|(?:viewtopic\.php.*p=)|(?:posting|search|ucp|viewonline|memberlist)\.php|hilit=' --tries 50 --visit-filter-is-download-filter | tee /tmp/slog
 cd $output_dir
-wget --page-requisites fusor.net/board/ --accept '*.css*' --output-document fusor.net/board/styles/prosilver/theme/stylesheet.css__q*
+wget --page-requisites fusor.net/board/ --accept '*.css*'
+cd fusor.net/board/styles/prosilver/theme/
+perl-rename -v 's/\?hash=.*//'  *css\?*
