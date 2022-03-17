53ef86e5654c63de74b34c0e556e61ccfcb38e78
raphTec <git@raphtec.net>
2022-03-17 14:25:44 +0100

    Update command to fix css

--- src/command.sh
+++ src/command.sh
@@ -1,1 +1,5 @@
-RUST_BACKTRACE=1 cargo run -- http://fusor.net --jobs 16 --verbose --output ~/fusor.net --exclude-download '(?:view=print)|(?:viewtopic\.php.*p=)|(?:posting|search|ucp|viewonline|memberlist)\.php|hilit=' --tries 50 --visit-filter-is-download-filter | tee /tmp/slog
+# this is ridiculous - how many programs do you need to archive a forum, fast?
+output_dir="$HOME/fusornet"
+RUST_BACKTRACE=1 cargo run -- http://fusor.net --jobs 16 --verbose --output $output_dir --exclude-download '(?:view=print)|(?:viewtopic\.php.*p=)|(?:posting|search|ucp|viewonline|memberlist)\.php|hilit=' --tries 50 --visit-filter-is-download-filter | tee /tmp/slog
+cd $output_dir
+wget --page-requisites fusor.net/board/ --accept '*.css*' --output-document fusor.net/board/styles/prosilver/theme/stylesheet.css__q*
